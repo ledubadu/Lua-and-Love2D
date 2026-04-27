92eacc49542782ae069221b2a42690a8b7b85590
ledubedu <ledubadu@gmail.com>
2026-04-27 14:17:38 +0600

    Create lua-basics.lua

---
+++ lua-basics/lua-basics.lua
@@ -0,0 +1,29 @@
+--[[ print("hello world")
+--  single line comment
+
+--[[
+    this is a 
+    multi line comment
+]]
+-- this , makes a tab in the middle
+--print("hello world", "too much tab")
+-- this .. is a concationation operator
+--print("hello".."world")
+
+-- tables are not necessary for love2d for now
+
+-- local is for this file and i can use this local variable anywhere in this lua-basics.lua file
+
+-- _G.Global or _g.global for global scope
+
+-- string
+--[[
+local x = [[asdas 
+asdfasfdg
+asdgasdg
+    asdfasdf
+        asdfasdf
+print (x) ]]
+
+
+
