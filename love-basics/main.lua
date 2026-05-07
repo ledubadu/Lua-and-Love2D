4fcad5853484023309b213f5033d77b0824b245f
ledubedu <ledubadu@gmail.com>
2026-05-07 22:04:27 +0600

    Update main.lua

--- love-basics/main.lua
+++ love-basics/main.lua
@@ -11,9 +11,9 @@ end
 
 function love.draw()
 	--love.graphics.print(number)
-	-- love.graphics.print("Hello, World!")
+	love.graphics.print("Hello, World!")
 	-- we have a squre
 	-- you have to have the folder for love
 	-- i really wish to use a game enigne but now it works
-	love.graphics.rectangle("fill", 100, 200, 50, 80)
+	--love.graphics.rectangle("fill", 100, 200, 50, 80)
 end
