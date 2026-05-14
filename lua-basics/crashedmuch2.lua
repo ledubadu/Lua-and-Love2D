63d6ba8d130a453ea660f772b39af4a577048ef3
ledubedu <ledubadu@gmail.com>
2026-05-14 16:55:05 +0600

    Update crashedmuch2.lua

--- lua-basics/crashedmuch2.lua
+++ lua-basics/crashedmuch2.lua
@@ -37,4 +37,16 @@ io.write ("5 * 3 = ", 5*3, "\n")
 io.write ("5 / 3 = ", 5/3, "\n")
 io.write ("5 % 3 = ", 5%3, "\n")
 
-io.write ("floor(2.345) : math
+
+-- Math funcitons: floor, ceil, max, min, sin, cos, tan,
+-- asin, acos, exp, log, log10, pow, sqrt, random, randomseed
+
+io.write ("floor(2.345) :", math.floor(2.345), "\n") -- rounded down
+io.write ("ceil(2.345) : ", math.ceil(2.345), "\n") -- rounded up
+io.write ("max(2,3) : ", math.max(2,3), "\n") -- max number selected
+io.write ("min(2,3) : ", math.min(2,3), "\n") -- minimum number selected
+io.write ("pow(8,2) : ", math.pow(8,2), "\n") -- 8 to the power of 2
+io.write ("sqrt(64) : ", math.sqrt(64), "\n") -- square root of 64
+
+
+ 
