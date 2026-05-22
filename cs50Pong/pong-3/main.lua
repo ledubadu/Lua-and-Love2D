da07e67897c7fe7b3d6f400409b61c6569b168d6
ledubedu <ledubadu@gmail.com>
2026-05-22 23:41:15 +0600

    pong 4 done

--- cs50Pong/pong-3/main.lua
+++ cs50Pong/pong-3/main.lua
@@ -1,73 +1,73 @@
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
+-- WINDOW_WIDTH = 1280
+-- WINDOW_HEIGHT = 720
 
-VIRTUAL_WIDTH = 432
-VIRTUAL_HEIGHT = 243
+-- VIRTUAL_WIDTH = 432
+-- VIRTUAL_HEIGHT = 243
 
-PADDLE_SPEED = 200
+-- PADDLE_SPEED = 200
 
-push = require("push")
+-- push = require("push")
 
-function love.load()
-	-- love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
-	-- 	fullscreen = false,
-	-- 	vsync = true,
-	-- 	resizable = false,
-	-- })
-	love.graphics.setDefaultFilter("nearest", "nearest")
+-- function love.load()
+-- 	-- love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+-- 	-- 	fullscreen = false,
+-- 	-- 	vsync = true,
+-- 	-- 	resizable = false,
+-- 	-- })
+-- 	love.graphics.setDefaultFilter("nearest", "nearest")
 
-	smallFont = love.graphics.newFont("font.ttf", 8)
-	scoreFont = love.graphics.newFont("font.ttf", 32)
+-- 	smallFont = love.graphics.newFont("font.ttf", 8)
+-- 	scoreFont = love.graphics.newFont("font.ttf", 32)
 
-	player1score = 0
-	player2score = 0
+-- 	player1score = 0
+-- 	player2score = 0
 
-	player1Y = 30
-	player2Y = VIRTUAL_HEIGHT - 40
-	-- love.graphics.setFont(smallFont)
+-- 	player1Y = 30
+-- 	player2Y = VIRTUAL_HEIGHT - 40
+-- 	-- love.graphics.setFont(smallFont)
 
-	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
-		fullscreen = false,
-		vsync = true,
-		resizable = false,
-	})
-end
+-- 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+-- 		fullscreen = false,
+-- 		vsync = true,
+-- 		resizable = false,
+-- 	})
+-- end
 
-function love.update(dt)
-	if love.keyboard.isDown("w") then
-		player1Y = player1Y - PADDLE_SPEED * dt
-	elseif love.keyboard.isDown("s") then
-		player1Y = player1Y + PADDLE_SPEED * dt
-	end
-	if love.keyboard.isDown("up") then
-		player2Y = player2Y - PADDLE_SPEED * dt
-	elseif love.keyboard.isDown("down") then
-		player2Y = player2Y + PADDLE_SPEED * dt
-	end
-end
+-- function love.update(dt)
+-- 	if love.keyboard.isDown("w") then
+-- 		player1Y = player1Y - PADDLE_SPEED * dt
+-- 	elseif love.keyboard.isDown("s") then
+-- 		player1Y = player1Y + PADDLE_SPEED * dt
+-- 	end
+-- 	if love.keyboard.isDown("up") then
+-- 		player2Y = player2Y - PADDLE_SPEED * dt
+-- 	elseif love.keyboard.isDown("down") then
+-- 		player2Y = player2Y + PADDLE_SPEED * dt
+-- 	end
+-- end
 
-function love.keypressed(key)
-	if key == "escape" then
-		love.event.quit()
-	end
-end
+-- function love.keypressed(key)
+-- 	if key == "escape" then
+-- 		love.event.quit()
+-- 	end
+-- end
 
-function love.draw()
-	push:apply("start")
+-- function love.draw()
+-- 	push:apply("start")
 
-	love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
-	love.graphics.setFont(smallFont)
+-- 	love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
+-- 	love.graphics.setFont(smallFont)
 
-	love.graphics.setFont(scoreFont)
-	love.graphics.print(player1score, VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
-	love.graphics.print(player2score, VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+-- 	love.graphics.setFont(scoreFont)
+-- 	love.graphics.print(player1score, VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+-- 	love.graphics.print(player2score, VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
 
-	love.graphics.rectangle("fill", VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 5, 5)
+-- 	love.graphics.rectangle("fill", VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 5, 5)
 
-	love.graphics.rectangle("fill", 5, player1Y, 5, 20)
-	love.graphics.rectangle("fill", VIRTUAL_WIDTH - 10, player2Y, 5, 20)
+-- 	love.graphics.rectangle("fill", 5, player1Y, 5, 20)
+-- 	love.graphics.rectangle("fill", VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
-	-- love.graphics.printf("Hello Pong!", 0, (VIRTUAL_HEIGHT / 2) - 6, VIRTUAL_WIDTH, "center")
-	love.graphics.printf("Hello Pong!", 0, 20, VIRTUAL_WIDTH, "center")
-	push:apply("end")
-end
+-- 	-- love.graphics.printf("Hello Pong!", 0, (VIRTUAL_HEIGHT / 2) - 6, VIRTUAL_WIDTH, "center")
+-- 	love.graphics.printf("Hello Pong!", 0, 20, VIRTUAL_WIDTH, "center")
+-- 	push:apply("end")
+-- end
---
+++ cs50Pong/pong-4/main.lua
@@ -0,0 +1,101 @@
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+PADDLE_SPEED = 200
+
+push = require("push")
+
+function love.load()
+	-- love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+	-- 	fullscreen = false,
+	-- 	vsync = true,
+	-- 	resizable = false,
+	-- })
+	math.randomseed(os.time())
+	-- love.graphics.setFont(smallFont)
+	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+		fullscreen = false,
+		vsync = true,
+		resizable = false,
+	})
+
+	love.graphics.setDefaultFilter("nearest", "nearest")
+
+	smallFont = love.graphics.newFont("font.ttf", 8)
+	scoreFont = love.graphics.newFont("font.ttf", 32)
+
+	player1score = 0
+	player2score = 0
+
+	player1Y = 30
+	player2Y = VIRTUAL_HEIGHT - 40
+
+	ballX = VIRTUAL_WIDTH / 2 - 2
+	ballY = VIRTUAL_HEIGHT / 2 - 2
+	ballDx = math.random(2) == 1 and -100 or 100
+	ballDy = math.random(-50, 50)
+
+	gameState = "start"
+end
+
+function love.update(dt)
+	if love.keyboard.isDown("w") then
+		player1Y = math.max(0, player1Y - PADDLE_SPEED * dt)
+	elseif love.keyboard.isDown("s") then
+		player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
+	end
+	if love.keyboard.isDown("up") then
+		player2Y = math.max(0, player2Y - PADDLE_SPEED * dt)
+	elseif love.keyboard.isDown("down") then
+		player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
+	end
+
+	if gameState == "play" then
+		ballX = ballX + ballDx * dt
+		ballY = ballY + ballDy * dt
+	end
+end
+
+function love.keypressed(key)
+	if key == "escape" then
+		love.event.quit()
+	elseif key == "enter" or key == "return" then
+		if gameState == "start" then
+			gameState = "play"
+		elseif gameState == "play" then
+			gameState = "start"
+			ballX = VIRTUAL_WIDTH / 2 - 2
+			ballY = VIRTUAL_HEIGHT / 2 - 2
+			ballDx = math.random(2) == 1 and -100 or 100
+			ballDy = math.random(-50, 50)
+			--ballX = ballX + ballDx * dt
+			--ballY = ballY + ballDy * dt
+		end
+	end
+end
+
+function love.draw()
+	push:apply("start")
+
+	love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
+	love.graphics.setFont(smallFont)
+	if gameState == "start" then
+		love.graphics.printf("Hello start Pong!", 0, 20, VIRTUAL_WIDTH, "center")
+	elseif gameState == "play" then
+		love.graphics.printf("Hello play Pong!", 0, 20, VIRTUAL_WIDTH, "center")
+	end
+	love.graphics.print(player1score, VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+	love.graphics.print(player2score, VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+
+	love.graphics.rectangle("fill", ballX, ballY, 5, 5)
+
+	love.graphics.rectangle("fill", 5, player1Y, 5, 20)
+	love.graphics.rectangle("fill", VIRTUAL_WIDTH - 10, player2Y, 5, 20)
+
+	-- love.graphics.printf("Hello Pong!", 0, (VIRTUAL_HEIGHT / 2) - 6, VIRTUAL_WIDTH, "center")
+
+	push:apply("end")
+end
