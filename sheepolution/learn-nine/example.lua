67f2d8992c5cd7402a581f50b2f642aeb0af686d
ledubedu <ledubadu@gmail.com>
2026-05-30 20:32:39 +0600

    learn-nine
    only a few more to go

---
+++ sheepolution/learn-nine/example.lua
@@ -0,0 +1,16 @@
+-- local test = 20
+-- if true then
+-- 	local test = 20
+-- end
+
+-- print (test)
+local test = 20
+function some_function(test)
+	if true then
+		local test = 40
+		print(test)
+	end
+	print(test)
+end
+some_function(30)
+print(test)
--- sheepolution/learn-nine/main.lua
+++ sheepolution/learn-nine/main.lua
@@ -1,1 +1,8 @@
 -- files and scopes
+--require("exmaple")
+--require("path.to.exmaple")
+--print(test)
+
+test = 10
+require("example")
+print(test)
