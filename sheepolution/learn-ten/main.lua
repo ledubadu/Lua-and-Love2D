c7ef2100069d448cb5b76e9cc2daf3706bada659
ledubedu <ledubadu@gmail.com>
2026-05-31 21:14:09 +0600

    learn-ten is done

--- sheepolution/learn-ten/main.lua
+++ sheepolution/learn-ten/main.lua
@@ -1,1 +1,21 @@
 -- libraries
+
+function love.load()
+	tick = require("tick")
+
+	drawRectangle = false
+
+	tick.delay(function()
+		drawRectangle = true
+	end, 2)
+end
+
+function love.update(dt)
+	tick.update(dt)
+end
+
+function love.draw()
+	if drawRectangle then
+		love.graphics.rectangle("fill", 100, 100, 300, 200)
+	end
+end
